681ae0299cfa093ca0886ce652f9cbf93e282de1
srappaport <sarah.rappaport@greendot.org>
2018-07-24 19:12:01 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -9,7 +9,6 @@ def change_rainbow_colors
   rainbow_colors[0] = "red"
   rainbow_colors[1] = "light_red"
   rainbow_colors[2]= "light_yellow"
-  puts rainbow_colors
   return rainbow_colors
 end
 change_rainbow_colors
